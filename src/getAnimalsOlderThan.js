b9a9b868484a13353c248abb54717ce99abd243c
Wallace <wallace_031@hotmail.com>
2022-10-31 16:14:43 -0300

    2. Implemente a função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const { species } = data;
+
+// console.log(species);
+
+const getAnimalsOlderThan = (animal, age) => {
+  const animalSearch = species.find((element) => element.name === animal);
+  return animalSearch.residents.every(({ age: age2 }) => age2 >= age);
+};
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
