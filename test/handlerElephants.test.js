dd17ca8c198c3e6b0affec39783e843a708dcb81
Wallace <wallace_031@hotmail.com>
2022-11-01 11:43:57 -0300

    6. Obtenha ao menos 80% de cobertura de testes na função handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,35 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se handlerElephants é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+
+  it('Testa se retorna undefine caso a função seja chamada sem parâmetro', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+
+  it('Testa o retorno caso o parâmetro não seja uma string', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Testa o retorno ao se passar uma string', () => {
+    expect(handlerElephants('animal')).toBe(null);
+  });
+
+  it('Testa o retorno ao se passar a string "count"', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('Testa o retorno ao se passar a string "names"', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  it('Testa o retorno ao se passar a string "averageAge"', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+
+  it('Testa o retorno ao se passar a string "location"', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+});
