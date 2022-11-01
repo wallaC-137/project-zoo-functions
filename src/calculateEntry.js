d3e3cff6adca8231e76c1a0b9e03975d4122fa82
Wallace <wallace_031@hotmail.com>
2022-11-01 19:28:09 -0300

    8. Implemente a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui;
-}
+const { prices } = data;
+const { adult, senior, child } = prices;
+
+const countEntrants = (entrants1) => {
+  const count = {
+    adult: 0,
+    senior: 0,
+    child: 0,
+  };
+  entrants1.forEach(({ age }) => {
+    if (age < 18) {
+      count.child += 1;
+    } else if (age >= 18 && age < 50) {
+      count.adult += 1;
+    } else {
+      count.senior += 1;
+    }
+  });
+  return count;
+};
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const newObj = countEntrants(entrants);
+  if (typeof entrants === 'object') {
+    return (newObj.adult * adult) + (newObj.senior * senior) + (newObj.child * child);
+  }
 }
 
 module.exports = { calculateEntry, countEntrants };
