0b651a25d7958670da587a26fd52f85ea035ca7d
Wallace <wallace_031@hotmail.com>
2022-11-02 08:04:37 -0300

    9. Implemente a função getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const { species } = data;
+const { hours } = data;
+
+const animalsDays = (param) => species.filter(({ availability }) => availability.includes(param))
+  .map(({ name }) => name);
+
+const createObjDays = (dia) => {
+  const dayKeys = Object.keys(hours);
+  const newObj = {};
+
+  dayKeys.forEach((day) => {
+    newObj[day] = { officeHour: (hours[day].open !== 0) ? `Open from ${
+      hours[day].open}am until ${hours[day].close}pm` : 'CLOSED',
+    exhibition: day === 'Monday' ? 'The zoo will be closed!' : animalsDays(day) };
+  });
+  return dayKeys.includes(dia) ? { [dia]: newObj[dia] } : newObj;
+};
+
+const getSchedule = (scheduleTarget) => {
+  if (typeof scheduleTarget === 'string') {
+    const testAnimal = species.find(({ name }) => name === scheduleTarget);
+    return testAnimal ? testAnimal.availability : createObjDays(scheduleTarget);
+  }
+  if (scheduleTarget === undefined) {
+    return createObjDays();
+  }
+};
 
 module.exports = getSchedule;
