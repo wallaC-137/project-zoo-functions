08707cf1febdcffb7efa5d8501782139dbb6e1c3
Wallace <wallace_031@hotmail.com>
2022-11-02 23:39:04 -0300

    11. Implemente a função getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees, species } = data;
+
+const funEmployees = () => {
+  const newObj = employees.map(({ id, firstName, lastName, responsibleFor: respo }) => ({
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: species.filter(({ id: id2 }) => respo.includes(id2)).map(({ name }) => name),
+    locations: species.filter(({ id: id2 }) => respo.includes(id2)).map(({ location }) => location),
+  }));
+  return newObj;
+};
+
+const getEmployeesCoverage = (searchName) => {
+  if (searchName === undefined) {
+    return funEmployees();
+  }
+
+  if (employees.find(({ id, lastName, firstName }) => id === searchName
+    .id || firstName === searchName.name || lastName === searchName.name) === undefined) {
+    throw new Error('Informações inválidas');
+  }
+
+  return funEmployees().find(({ id, fullName }) => id === searchName.id || fullName
+    .split(' ').includes(searchName.name));
+};
 
 module.exports = getEmployeesCoverage;
